13846cfc347d44153fc346a41520cd54ef7521e2
Bahauddin-Fahad <bahauddin.fahad819@gmail.com>
2022-03-01 21:48:50 +0600

    details section added

--- js/app.js
+++ js/app.js
@@ -5,45 +5,66 @@ const clearDetails = (id) => {
 
 // Getting the search Text //
 const searchValue = () => {
+  const main = document.getElementById("website-elements");
+  const emptyErrorMessage = document.getElementById("empty-error-message");
+  const totalCount = document.getElementById("total-count");
   const searchField = document.getElementById("search-button");
   const searchFieldValue = searchField.value;
-  const url = `https://openapi.programming-hero.com/api/phones?search=${searchFieldValue}`;
-  fetch(url)
-    .then((res) => res.json())
-    .then((phones) => displayPhone(phones.data));
-  clearDetails("main-details");
-  clearDetails("other-details");
-  // console.log(searchField.value);
-  searchField.value = "";
+  if (searchFieldValue === "" || searchFieldValue <= 0) {
+    emptyErrorMessage.style.display = "block";
+    totalCount.style.display = "none";
+    main.style.display = "none";
+  } else {
+    emptyErrorMessage.style.display = "none";
+    totalCount.style.display = "block";
+    main.style.display = "block";
+    const url = `https://openapi.programming-hero.com/api/phones?search=${searchFieldValue}`;
+    fetch(url)
+      .then((res) => res.json())
+      .then((phones) => displayPhone(phones.data));
+    clearDetails("main-details");
+    clearDetails("other-details");
+    // console.log(searchField.value);
+    searchField.value = "";
+  }
 };
 
 // Displaying the Phone List
 const displayPhone = (phones) => {
   // console.log(phones);
-  const errorMessage = document.getElementById("error-message");
+  const main = document.getElementById("website-elements");
+  const wrongErrorMessage = document.getElementById("wrong-error-message");
   const totalCount = document.getElementById("total-count");
   const showMoreButton = document.getElementById("show-more-phones");
-  if (phones.length == 0) {
-    errorMessage.style.display = "block";
+  if (phones.length === 0) {
+    wrongErrorMessage.style.display = "block";
+    totalCount.style.display = "none";
+    main.style.display = "none";
     clearDetails("phone-details");
     clearDetails("phone-lists");
     clearDetails("total-count");
     showMoreButton.style.display = "none";
   } else {
-    errorMessage.style.display = "none";
+    wrongErrorMessage.style.display = "none";
+    totalCount.style.display = "block";
+    main.style.display = "block";
     totalCount.textContent = `Showing ${phones.length} results`;
     clearDetails("phone-lists");
     if (phones.length < 20) {
       showMoreButton.style.display = "none";
+      clearDetails("phone-lists");
       showResults(phones);
     } else if (phones.length > 20) {
+      clearDetails("phone-lists");
       showMoreButton.style.display = "block";
+      clearDetails("phone-lists");
       const phoneList = phones.slice(0, 20);
       showResults(phoneList);
       document
         .getElementById("show-more-phones")
         .addEventListener("click", function () {
           showMoreButton.style.display = "none";
+          clearDetails("phone-lists");
           showResults(phones);
         });
     }
@@ -94,12 +115,16 @@ const showDetails = (data) => {
   const mainDetailsDiv = document.createElement("div");
   mainDetailsDiv.classList.add("detail-box");
   mainDetailsDiv.innerHTML = `
-   <h5>Main Features of ${data.name}</h5>
-            <h6>Storage: ${data.mainFeatures.storage}</h6>
-            <h6>Display Size: ${data.mainFeatures.displaySize} </h6>
-            <h6>Chipset:  ${data.mainFeatures.chipSet}</h6>
-            <h6>Memory:  ${data.mainFeatures.memory}</h6>
-            <h6>Sensors:  ${data.mainFeatures.sensors}</h6>`;
+            <div class="text-center mx-auto "style="padding: 20px">
+              <img src="${data.image}" class="image-border" alt="" />
+            </div>
+            <h6 class="text-center"> ${data.releaseDate} <h6>
+            <h4 class="features-tag">Main Features of ${data.name}</h4>
+            <h6 class="details-tag"><span class="features-tag">Storage :</span> ${data.mainFeatures.storage}</h6>
+            <h6 class="details-tag"><span class="features-tag">Display Size :</span> ${data.mainFeatures.displaySize} </h6>
+            <h6 class="details-tag"><span class="features-tag">Chipset :</span>  ${data.mainFeatures.chipSet}</h6>
+            <h6 class="details-tag"><span class="features-tag">Memory :</span>  ${data.mainFeatures.memory}</h6>
+            <h6 class="details-tag"><span class="features-tag">Sensors :</span>  ${data.mainFeatures.sensors}</h6>`;
   mainDetailBox.appendChild(mainDetailsDiv);
 
   // displaying the other details//
@@ -110,13 +135,13 @@ const showDetails = (data) => {
     const otherDetailsDiv = document.createElement("div");
     otherDetailsDiv.classList.add("detail-box");
     otherDetailsDiv.innerHTML = `
-   <h5>Other Features Of ${data.name}</h5>
-            <h6>WLAN: ${data.others.WLAN}</h6>
-            <h6>Bluetooth: ${data.others.Bluetooth} </h6>
-            <h6>GPS: ${data.others.GPS}</h6>
-            <h6>USB: ${data.others.USB}</h6>
-            <h6>Radio: ${data.others.Radio}</h6>
-            <h6>NFC: ${data.others.NFC}</h6>`;
+          <h4 class="features-tag">Other Features Of ${data.name}</h4>
+            <h6 class="details-tag"><span class="features-tag">WLAN :</span> ${data.others.WLAN}</h6>
+            <h6 class="details-tag"><span class="features-tag">Bluetooth :</span> ${data.others.Bluetooth} </h6>
+            <h6 class="details-tag"><span class="features-tag">GPS :</span> ${data.others.GPS}</h6>
+            <h6 class="details-tag"><span class="features-tag">USB :</span> ${data.others.USB}</h6>
+            <h6 class="details-tag"><span class="features-tag">Radio :</span> ${data.others.Radio}</h6>
+            <h6 class="details-tag"><span class="features-tag">NFC :</span> ${data.others.NFC}</h6>`;
     otherDetailBox.appendChild(otherDetailsDiv);
   } else {
     otherDetailBox.removeAttribute("class");
